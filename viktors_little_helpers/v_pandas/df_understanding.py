986e8662bc88f0b5ec23e78b8888caabd7003c93
ViktoV <vikschneid@gmail.com>
2023-02-10 10:37:40 +0100

    added sentiment analysis and decision tree. added df understanding function to check the columns of a data frame for different data types

---
+++ viktors_little_helpers/v_pandas/df_understanding.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+def df_quick_check(df):
+    # Check and count each column for:
+    results_dict = {"Name": []
+                    , "Missing Values": []
+                    , "Numerical Values": []
+                    , "Only Alphabetical Values": []
+                    , "Alphanumerical Values": []
+                    , "Values with Special Characters": []
+                    , "Date Values": []
+                   }
+    
+    for c in df.columns:
+        results_dict["Name"].append(c)
+        results_dict["Missing Values"].append(sum(df[c].isna()))
+        results_dict["Numerical Values"].append(sum([str(x).isdigit() for x in list(df[c])]))
+        results_dict["Only Alphabetical Values"].append(sum([str(x).isalpha() for x in list(df[c]) if str(x) != "nan"]))
+        results_dict["Alphanumerical Values"].append(sum([str(x).isalnum() for x in list(df["purchase_price"]) if str(x) != "nan"])) # exclude whitespace
+        results_dict["Values with Special Characters"].append(sum([not str(x).isalnum() for x in list(df["purchase_price"]) if str(x) != "nan"])) # values with special characters
+        results_dict["Date Values"].append(sum(df[c].str.contains("^\d{4}-\d{2}-\d{2}").fillna(False))) # date values
+    
+    return pd.DataFrame.from_dict(results_dict, orient="index", columns=results_dict["Name"]).drop("Name")
